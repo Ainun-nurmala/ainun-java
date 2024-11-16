d63f876200eb5d1b52ae2748c3d9efc5a8114543
Ainun-nurmala <nurmalsariainun@gmail.com>
2024-11-16 13:00:05 +0700

    Add files via upload

---
+++ ltihan.java
@@ -0,0 +1,18 @@
+// java string length
+public String buah{
+  public static void main(String[] args){
+   String buah ="anggur";
+    System.out.println(buah.length())
+  }
+}
+
+//java string is empty
+public clas hewan{
+  public static void main(String[]args){
+  
+  String hewan1="ayam";
+  String hewan2="anjing";
+  System.out.println(nama1.is empty());
+  System.out.println(nama2.is empty());
+  }
+}
